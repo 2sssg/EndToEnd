a30e06ad9bc935cfeb166945a3cb537fc8a9b9ef
2sssg <lsg020300@naver.com>
2021-11-20 23:12:44 +0900

    ?

--- src/main/java/com/javaproject/endtoend/EndToEndApplication.java
+++ src/main/java/com/javaproject/endtoend/EndToEndApplication.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EndToEndApplication {
 
-    public static void main(String[] args) {
-
-
-
-        SpringApplication.run(EndToEndApplication.class, args);
-    }
+    public static void main(String[] args) {SpringApplication.run(EndToEndApplication.class, args);}
 
 }
