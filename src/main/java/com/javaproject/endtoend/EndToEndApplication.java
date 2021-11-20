f5978e9c91e26209f6e7624287b41e7b8ca0315e
2sssg <73799865+2sssg@users.noreply.github.com>
2021-11-20 16:47:01 +0900

    Feat: Default Project
    Default Project File 
    (Just Setting)

---
+++ src/main/java/com/javaproject/endtoend/EndToEndApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.endtoend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndToEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndToEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaproject/endtoend/EndToEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.endtoend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndToEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
