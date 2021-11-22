3ce7729ce075666256264d2b2fe82368e98e68ec
2sssg <lsg020300@naver.com>
2021-11-23 02:25:55 +0900

    Feat: MainController.class
    MainController
    TODO) Code Refactoring , must have to file SEPARATED

---
+++ src/main/java/com/javaproject/endtoend/controller/MainController.java
@@ -0,0 +1,122 @@
+package com.javaproject.endtoend.controller;
+
+import com.javaproject.endtoend.DTO.RoomInUser;
+import com.javaproject.endtoend.model.Room;
+import com.javaproject.endtoend.model.User;
+import com.javaproject.endtoend.repository.RoomRepository;
+import com.javaproject.endtoend.repository.UserRepository;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@Service
+public class MainController {
+
+    @Autowired
+    RoomRepository roomRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+
+
+
+
+    @Transactional
+    @RequestMapping()
+    public String mainController(Model model){
+
+        List<Room> rooms = roomRepository.findByFroomsearching(1);
+        model.addAttribute("rooms",rooms);
+
+        model.addAttribute("roomInUser",new RoomInUser());
+        System.out.println(model);
+        return "index";
+    }
+
+    @Transactional
+    @RequestMapping(value = "/gameIn")
+    public String loginController(User user, Model model){
+        Room room;
+        user.setUserName("임시");
+        System.out.println("gameIn");
+        if(roomRepository.countByFroomsearching(1)>0){
+            room = roomRepository.findOneByFroomsearching(1).orElseThrow();
+            room.setFroomsearching(0);
+            room.setSecondUser(user);
+        }else{
+            room = Room.builder()
+                    .froomsearching(1)
+                    .roomNum(new Date().hashCode())
+                    .firstUser(user)
+                    .build();
+        }
+        userRepository.save(user);
+        roomRepository.save(room);
+        model.addAttribute("room", room);
+
+        System.out.println("dddddd");
+        return "gameIn";
+    }
+
+
+    @RequestMapping("/routemakeroom")
+    public String routemakeRoomController(Model model, RoomInUser roomInUser){
+        return "makeroom";
+    }
+
+    @Transactional
+    @RequestMapping("/makeroom")
+    public String makeRoomController(Model model, RoomInUser roomInUser){
+
+        User user = User.builder()
+                .userName(roomInUser.getUserName())
+                .build();
+
+        Room room = Room.builder()
+                .froomsearching(1)
+                .roomName(roomInUser.getRoomName())
+                .roomNum(new Date().hashCode())
+                .firstUser(user)
+                .build();
+
+        userRepository.save(user);
+        roomRepository.save(room);
+        model.addAttribute("room", room);
+
+        return "gameIn";
+    }
+
+    @Transactional
+    @RequestMapping("/specificroom")
+    public String specificroomController(Model model,RoomInUser roomInUser){
+
+
+        System.out.println("dasdas"+roomInUser);
+
+        User user = User.builder()
+                .userName(roomInUser.getUserName())
+                .build();
+
+        Room room = roomRepository.findByRoomNum(roomInUser.getRoomNum());
+        room.setSecondUser(user);
+        room.setFroomsearching(0);
+        userRepository.save(user);
+        roomRepository.save(room);
+        model.addAttribute(room);
+        return "gameIn";
+    }
+
+
+
+
+}
