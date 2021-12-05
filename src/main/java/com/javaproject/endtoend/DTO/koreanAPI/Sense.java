3b97a62709a091a6b970128c15dc29d4ad3e7b47
2sssg <73799865+2sssg@users.noreply.github.com>
2021-12-05 14:07:05 +0900

    Feat: send DTO
    for KoreanAPI

---
+++ src/main/java/com/javaproject/endtoend/DTO/koreanAPI/Sense.java
@@ -0,0 +1,15 @@
+package com.javaproject.endtoend.DTO.koreanAPI;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Sense {
+    private String definition;
+    private String link;
+    private String type;
+}
